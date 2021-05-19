8286701fc3f99bd97131e4d2e37984ff4851a834
Alexander Naymov <naymov.branch@gmail.com>
2021-05-19 12:52:01 +0300

    empty project

---
+++ RickAndMorty/Screens/MainModule/View/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  RickAndMorty
+//
+//  Created by coder on 19.05.21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
